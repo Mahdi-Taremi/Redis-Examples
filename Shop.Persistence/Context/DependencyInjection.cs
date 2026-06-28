057a052ef2d24d5e1e6dcb28d52d93583c5a4bc7
MT <taremiam@gmail.com>
2026-06-28 15:58:45 +0330

    feat(application): introduce IApplicationDbContext abstraction and Config Persistence.

--- Shop.Persistence/Context/DependencyInjection.cs
+++ Shop.Persistence/Context/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,14 @@ namespace Shop.Persistence.Context
         this IServiceCollection services,
          IConfiguration configuration)
         {
-            services.AddDbContext<ShopDbContext>(options =>
-            {
-                options.UseSqlServer(
-                    configuration.GetConnectionString("Local"));
-            });
+            //services.AddDbContext<ShopDbContext>(options =>
+            //{
+            //    options.UseSqlServer(
+            //        configuration.GetConnectionString("Local"));
+            //});
+            services.AddScoped<IApplicationDbContext>(
+            provider =>
+            provider.GetRequiredService<ShopDbContext>());
 
             return services;
         }
--- Shop.Persistence/Context/ShopDbContext.cs
+++ Shop.Persistence/Context/ShopDbContext.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Application.Interfaces;
+using Shop.Domain.Entities;
 using Shop.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Shop.Domain.Entities;
 
 namespace Shop.Persistence.Context
 {
-    public class ShopDbContext : DbContext
+    public class ShopDbContext : DbContext,IApplicationDbContext
     {
         public ShopDbContext(DbContextOptions<ShopDbContext> options):base(options)
         {
