70f0ed4638e64b43f1a5c04cef874fa84cc41826
MT <taremiam@gmail.com>
2026-06-28 16:17:26 +0330

    refactor(domain): introduce BaseEntity and BaseAuditableEntity

---
+++ Shop.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+
+        protected BaseEntity()
+        {
+              Id = Guid.NewGuid();
+            //Use .NET 9
+            //Id = Guid.CreateVersion7();
+        }
+    }
+}
--- Shop.Domain/Entities/Product.cs
+++ Shop.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop.Domain.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace Shop.Domain.Entities
 {
-        public class Product : BaseEntity
-        {
-            public string Name { get; set; }
+    public class Product : BaseAuditableEntity
+    {
+        public string Name { get; set; } = default!;
 
-            public decimal Price { get; set; }
+        public decimal Price { get; set; }
 
-            public int Stock { get; set; }
-        }
+        public int Stock { get; set; }
+    }
 }
